ac7005aff6072c232523a985f57e09a7474176ed
AbdelrahmanElkhradely <65429772+AbdelrahmanElkhradely@users.noreply.github.com>
2022-03-08 15:08:21 -0500

    Update Login.jsx

--- backend/frontend/src/components/Login.jsx
+++ backend/frontend/src/components/Login.jsx
@@ -19,6 +19,8 @@ const Login = () => {
     };
 
     const response = await fetch("/api/token", requestOptions);
+    console.log(response)
+    
     const data = await response.json();
 
     if (!response.ok) {
